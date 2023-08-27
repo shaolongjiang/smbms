ae67db6d2ac21384c553554270729b720ee718ba
longjaing <2697446954@qq.com>
2023-08-27 15:53:46 +0800

    dev第一次测试

--- src/main/java/com/shao/util/Constants.java
+++ src/main/java/com/shao/util/Constants.java
@@ -3,4 +3,5 @@ package com.shao.util;
 public class Constants {
     public final static  String USER_SESSION = "userSession";
     public final static  int PageSize = 5;
+    public static int d = 2;
 }
